e26bced6ebbcd2bae558b40698a3dff2dcfc5d6d
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-11-15 22:52:15 +0200

    update

--- contacts.js
+++ contacts.js
@@ -1,22 +1,32 @@
-const fs = require("fs/promises")
-const path = require("path")
+const fs = require("fs/promises");
+const path = require("path");
 
 // Розкоментуй і запиши значення
-const contactsPath = path.join(__dirname, './db/contacts.json');
+// const contactsPath = ;
 
 // TODO: задокументувати кожну функцію
 function listContacts() {
-    // ...твій код. Повертає масив контактів.
-  }
-  
-  function getContactById(contactId) {
-    // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-  }
-  
-  function removeContact(contactId) {
-    // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-  }
-  
-  function addContact(name, email, phone) {
-    // ...твій код. Повертає об'єкт доданого контакту. 
-  }
+  // ...твій код. Повертає масив контактів.
+}
+
+function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту.
+}
+
+//============================
+
+const name = ["Alex", "Sofi", "Mejson", "Gibson"];
+const age = [23, 45, 67, 98];
+const object = {
+  name,
+  age,
+};
+module.exports = object;
---
+++ index.js
@@ -0,0 +1,6 @@
+//==================================
+console.log("====Welcome to Node.js====");
+
+const obj = require("./contacts");
+console.log(obj.name);
+console.log(obj.age);
